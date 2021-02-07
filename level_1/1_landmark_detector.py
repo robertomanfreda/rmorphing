ab3fd5e1b5b18bb68af1cfe54968ae1640daf0a7
Roberto Manfreda <robertomanfreda1994@gmail.com>
2021-02-07 16:22:38 +0100

    [FIX] Fixed a copyright issue

--- level_1/1_landmark_detector.py
+++ level_1/1_landmark_detector.py
@@ -1,23 +1,30 @@
 from __future__ import print_function
 import dlib
 import cv2 as cv
+from imutils import face_utils
 import sys
 
 
 PREDICTOR_PATH = '../opencv_utils/predictors/shape_predictor_68_face_landmarks.dat'
 FRAME_NAME = 'level_1 - landmark_detector'
 
-faceDetector = dlib.get_frontal_face_detector()
-landmarkDetector = dlib.shape_predictor(PREDICTOR_PATH)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
 def detect_and_display(frame):
     frameGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frameGray = cv.equalizeHist(frameGray)
 
-    points = fbc.getLandmarks(faceDetector, landmarkDetector, frameGray)
+    rects = detector(frameGray, 1)
 
-    for point in points:
-        cv.circle(frame, point, radius=1, color=(255, 255, 0), thickness=1)
+    for (i, rect) in enumerate(rects):
+        shape = predictor(frameGray, rect)
+        shape = face_utils.shape_to_np(shape)
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        for (x, y) in shape:
+            cv.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
     cv.imshow(FRAME_NAME, frame)
 
